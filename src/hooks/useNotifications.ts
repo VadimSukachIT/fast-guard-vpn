242ca34233051c92873ec438d752b8b7d79c4b1a
vadimsukach <vadimsukach@MacBook-Pro-vadimsukach.local>
2025-06-04 16:19:33 +0300

    added more logs

--- src/hooks/useNotifications.ts
+++ src/hooks/useNotifications.ts
@@ -17,7 +17,7 @@ export const useNotifications = () => {
   useEffect(() => {
     const initNotifications = async () => {
       console.log('[OneSignal] Init called');
-      console.log('Use effect : ', import.meta.env.VITE_VERCEL_ENV);
+      console.log('Use effect env: ', import.meta.env.VITE_VERCEL_ENV);
       await OneSignal.init({
         appId: import.meta.env.VITE_VERCEL_EN === 'production' ? ONE_SIGNAL_TEST_APP_ID : ONE_SIGNAL_APP_ID,
         serviceWorkerPath: 'service-worker.js',
