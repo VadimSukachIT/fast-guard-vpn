f5aadc92584a7b8001e1d7137fdaee9b854af843
vadimsukach <vadimsukach@MacBook-Pro-vadimsukach.local>
2025-06-04 17:41:38 +0300

    another vibration test

--- src/hooks/useVibration.ts
+++ src/hooks/useVibration.ts
@@ -4,27 +4,19 @@ export const useLoopVibration = () => {
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
-    console.log('effect triggered');
-    const isAndroidStandalone = () =>
-      /android/i.test(navigator.userAgent) &&
-      (window.matchMedia("(display-mode: standalone)").matches || (navigator as any).standalone);
-
-    const tick = () => {
-      if ("vibrate" in navigator) {
-        if (isAndroidStandalone()) {
-          navigator.vibrate(30);
-        }
-      }
+    const handle = () => {
+      navigator.vibrate?.(30); // разрешаем
+  
+      intervalRef.current = setInterval(() => {
+        navigator.vibrate?.(30);
+      }, 1000);
+  
+      document.removeEventListener("pointerdown", handle);
     };
-
-    // Стартуем немедленно, потом каждые 1000 мс
-    tick();
-    intervalRef.current = setInterval(() => {
-      tick();
-    }, 1000);
-
+  
+    document.addEventListener("pointerdown", handle, { once: true, passive: true });
+  
     return () => {
-      console.log('clear triggered');
       if (intervalRef.current) clearInterval(intervalRef.current);
     };
   }, []);
