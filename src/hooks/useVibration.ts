c01b2aaa3f83531b43ab29fffa7ae8df07c622c1
vadimsukach <vadimsukach@MacBook-Pro-vadimsukach.local>
2025-06-05 15:52:35 +0300

    fix crash

--- src/hooks/useVibration.ts
+++ src/hooks/useVibration.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
 export const useLoopVibration = () => {
   const buttonRef = useRef<HTMLButtonElement | null>(null);
