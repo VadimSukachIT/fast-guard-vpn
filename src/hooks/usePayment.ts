e6ec2cbfa22dc17d3f2e7d60ebd84bf00293adda
vadimsukach <vadimsukach@MacBook-Pro-vadimsukach.local>
2025-06-07 11:38:03 +0300

    add logs

--- src/hooks/usePayment.ts
+++ src/hooks/usePayment.ts
@@ -8,7 +8,6 @@ export const usePayment = (subID: number, isFreeTrial?: boolean) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [conf] = useLocalStorage('conf', 0);
  
-  console.log(conf, typeof conf);
   const getSubscriptionId = () => {
     return isFreeTrial ? subID - 1 : subID;
   }
@@ -18,18 +17,22 @@ export const usePayment = (subID: number, isFreeTrial?: boolean) => {
     const pwaId = localStorage.getItem('pwaId');
     const clid = localStorage.getItem('clid');
     const subscriptionId = getSubscriptionId();
-    let oneSignalID = OneSignal.User.onesignalId || '';
+    const oneSignalID = OneSignal.User.onesignalId || '';
 
      await sendEvent(`/event?event=paymentopen&clid=${clid}&pwaID=${pwaId}&onesignalID=${oneSignalID}&subID=${subID}`)
 
      const isStandalone =  window.matchMedia('(display-mode: standalone)').matches ||
      (window.navigator as any).standalone === true;
-     
+
+     console.log('Is standalone: ', isStandalone);
+     console.log(conf, typeof conf);
      const paymentLink = `https://ray.yourmessage.me/v1.0/user/billing/flow/web/yookassa/subscription/create?subscriptionId=${subscriptionId}&pwaId=${pwaId}&clickId=${clid}&onesignalID=${oneSignalID}&source=pwa&urlOk=https://hide-vpn.com?payment-success=true&urlFail=https://hide-vpn.com?payment-success=false`;
      if (isStandalone && conf === 1) {
+      console.log('window.open blank');
        window.open(paymentLink, '_blank');
      } else {
-       window.location.href = paymentLink;
+        console.log('default');
+        window.location.href = paymentLink;
      }
 
 
