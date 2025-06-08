bbe120e8d69cfcbc509fbcdc71b544f6ba94222c
vadimsukach <vadimsukach@MacBook-Pro-vadimsukach.local>
2025-06-08 22:52:22 +0300

    add payment link

--- src/hooks/usePayment.ts
+++ src/hooks/usePayment.ts
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { useEvent } from "./useEvent";
 import OneSignal from "react-onesignal";
-// import { useLocalStorage } from "usehooks-ts";
+import { useLocalStorage } from "usehooks-ts";
 
 export const usePayment = (subID: number, isFreeTrial?: boolean) => {
   const { sendEvent } = useEvent();
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  // const [conf] = useLocalStorage('conf', 0);
+  const [conf] = useLocalStorage('conf', 0);
  
   const getSubscriptionId = () => {
     return isFreeTrial ? subID - 1 : subID;
@@ -19,20 +19,19 @@ export const usePayment = (subID: number, isFreeTrial?: boolean) => {
     const subscriptionId = getSubscriptionId();
     const oneSignalID = OneSignal.User.onesignalId || '';
 
-    await sendEvent(`/event?event=paymentopen&clid=${clid}&pwaID=${pwaId}&onesignalID=${oneSignalID}&subID=${subID}`)
-    const urlOk = encodeURIComponent('https://hide-vpn.com?payment-success=true');
-    const urlFail = encodeURIComponent('https://hide-vpn.com?payment-success=false');
-    //  const isStandalone =  window.matchMedia('(display-mode: standalone)').matches ||
-    //  (window.navigator as any).standalone === true;
+     await sendEvent(`/event?event=paymentopen&clid=${clid}&pwaID=${pwaId}&onesignalID=${oneSignalID}&subID=${subID}`)
 
-    //  const paymentLink = `https://ray.yourmessage.me/v1.0/user/billing/flow/web/yookassa/subscription/create?subscriptionId=${subscriptionId}&pwaId=${pwaId}&clickId=${clid}&onesignalID=${oneSignalID}&source=pwa&urlOk=https://hide-vpn.com?payment-success=true&urlFail=https://hide-vpn.com?payment-success=false`;
-    const paymentLink = `https://ray.yourmessage.me/v1.0/user/billing/flow/web/yookassa/subscription/create?subscriptionId=${subscriptionId}&pwaId=${pwaId}&clickId=${clid}&onesignalID=${oneSignalID}&source=pwa&urlOk=${urlOk}&urlFail=${urlFail}`;
+     const isStandalone =  window.matchMedia('(display-mode: standalone)').matches ||
+     (window.navigator as any).standalone === true;
 
-     window.open(paymentLink, '_blank', 'noopener,noreferrer');
-    //  if (isStandalone && conf === 1) {
-
-        // window.location.href = paymentLink;
-    //  }
+     const paymentLink = `https://ray.yourmessage.me/v1.0/user/billing/flow/web/yookassa/subscription/create?subscriptionId=${subscriptionId}&pwaId=${pwaId}&clickId=${clid}&onesignalID=${oneSignalID}&source=pwa&urlOk=https://hide-vpn.com?payment-success=true&urlFail=https://hide-vpn.com?payment-success=false`;
+     if (isStandalone && conf === 1) {
+      console.log('window.open blank');
+      window.open(paymentLink, '_blank', 'noopener,noreferrer');
+     } else {
+        console.log('default')
+        window.location.href = paymentLink;
+     }
 
 
     setTimeout(() => {
